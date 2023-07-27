a1b1562071f5e4cf5a479b0f16f8b21d0a548f05
david-aryasetia <david.aryasetia@gmail.com>
2023-07-27 08:47:47 +0700

    testing environtment

---
+++ firstProgram.cpp
@@ -0,0 +1,13 @@
+/*
+Double multiple comment started program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World! \n"; // c++ with new line
+    cout << "Im David " << endl  ;
+    cout << "Hello I'm David" << endl; // This is a comment
+    return 0;
+}
