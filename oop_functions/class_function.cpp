e116cd7f6b1d65341e53d8fece4287eba00b7f1b
David Aryasetia <99853366+davidaryasetia@users.noreply.github.com>
2023-10-13 10:43:28 +0700

    Update class_function.cpp

--- oop_functions/class_function.cpp
+++ oop_functions/class_function.cpp
@@ -25,15 +25,32 @@ class Motorcycle {
     }
 };
 
+class Bycycle {
+public : 
+string merk;
+string color;
+string type;
+
+void description_bycycle(string merk, string color, string type){
+    cout << "My Bycycle merk : " << merk << ", type : " << type << "color: " << color << "\n"; 
+}
+}
+
 int main (){
     Car Vehicle_Car;
     Motorcycle Vehicle_Motor;
-    
+    Bycycle Vehicle_Bycycle;
+
+    // Vehicle Car
     Vehicle_Car.description_car("BMW", "i5", 1200, "Red");
     Vehicle_Car.description_car("Toyota", "Innova", 1000, "White");
     Vehicle_Car.merk="Mitsubishi";
     Vehicle_Car.color="Black";
     Vehicle_Car.type="Pajero Sport";
+
+    // Vehicle Bycycle 
+    Vehicle_Bycycle.description_bycycle("United", "red", "Mountain Bike");
+    
     cout << Vehicle_Car.merk << " " << Vehicle_Car.type << " " << Vehicle_Car.color << "\n";
     return 0;
 }
